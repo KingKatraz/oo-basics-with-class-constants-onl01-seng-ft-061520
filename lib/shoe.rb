809483f6f871bdf94cd6ad674a7c9f20404528c7
Hal H Nader <halnader89@gmail.com>
2020-06-29 06:06:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,14 @@
 class Shoe
-  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  
-  BRANDS
+
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand 
+    end
   end
 
   def cobble
